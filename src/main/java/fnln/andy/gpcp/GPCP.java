a25d80c187845ad0042f160ddafffa41bc3e1333
fnlnandy <andyfaniloniaina01@gmail.com>
2024-05-09 08:52:35 +0300

    Initial commit

---
+++ src/main/java/fnln/andy/gpcp/GPCP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fnln.andy.gpcp;
+
+/**
+ *
+ * @author andy
+ */
+public class GPCP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
